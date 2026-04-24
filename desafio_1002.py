975cdd3d521133e3dc80324ef4123de50d95d7d7
Taiiyou <ggs.gustavo08@gmail.com>
2026-04-24 10:39:34 -0300

    Update area calculation and print format
    Refactor area calculation and output format.

--- desafio_1002.py
+++ desafio_1002.py
@@ -1,26 +1,5 @@
-"""
-Beecrowd 1002 - Área do Círculo
-
-A fórmula para calcular a área de uma circunferência é: area = π . raio².
-Considerando para este problema que π = 3.14159:
-
-- Efetue o cálculo da área, elevando o valor de raio ao quadrado e
-  multiplicando por π.
-
-Entrada: O arquivo de entrada contém um valor de ponto flutuante (dupla precisão),
-no caso, a variável raio.
-
-Saída: Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme
-exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão
-(double).
-"""
-
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1002
-
-# Escreva sua solução abaixo
 raio = float(input())
 
-pi = 3.14159
-area = pi * (raio ** 2)
+area = 3.14159 * (raio ** 2)
 
-print("A={:.4f}".format(area))
+print(f"A={area:.4f}")
